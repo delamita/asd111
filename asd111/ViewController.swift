f65906d9357e0bfea42e87e1c1c79bfe7b3319ac
费克翔 <delamitaduke@icloud.com>
2019-01-25 15:59:35 +0800

    Initial Commit

---
+++ asd111/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asd111
+//
+//  Created by 费克翔 on 2019/1/25.
+//  Copyright © 2019 徐夷琦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ asd111Tests/asd111Tests.swift
@@ -0,0 +1,34 @@
+//
+//  asd111Tests.swift
+//  asd111Tests
+//
+//  Created by 费克翔 on 2019/1/25.
+//  Copyright © 2019 徐夷琦. All rights reserved.
+//
+
+import XCTest
+@testable import asd111
+
+class asd111Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
